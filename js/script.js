75079d25b21d7719fc88dfbe9b82106b4e7e7702
Sahil-Dhillon <Sahil-Dhillon@users.noreply.github.com>
2021-02-04 23:24:06 +0530

    d

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function navtoggle(x) {
     x.classList.toggle("change");
     document.getElementsByClassName('tnav')[0].classList.toggle("active");
 }
+
+
+window.addEventListener('scroll', function() {
+    value = window.scrollY;
+
+    bg = document.querySelector('#bg');
+    bg.style.backgroundSize = 100 + value * 2 + "px";
+    if (value === 1333) {
+
+        s1 = document.querySelector('.s1');
+        // s1.style.position = 'absolute'
+        s1.style.overflow = 'auto'
+
+
+    }
+})
